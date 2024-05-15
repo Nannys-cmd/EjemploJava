7036b048fe4c58d0b203e3d93f721693346324e0
Daniela Bastias <danybastias@outlook.com>
2024-05-15 15:11:05 -0300

    Se agrego un cartel en el main

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Contacto contacto1 = new Contacto()(nombre "Romina", telefono "2341556743");
         System.out.println(contacto1.getNombre());
+        System.out.println("ESTO ES UN CARTEL");
     }
 }
