ec4d614b68966836995bc40622fce4fa08209e7c
Daniela Bastias <danybastias@outlook.com>
2024-05-16 14:57:51 -0300

    Probando

--- src/Contacto.java
+++ src/Contacto.java
@@ -15,5 +15,15 @@ public class Contacto {
     public void setTelefono(String telefono) { this.telefono = telefono; }
     public String getCorreo() { return correo; }
     public void setCorreo(String correo) { this.correo = correo; }
-    
+    public String getDireccion() { return direccion; }
+    public void  setDireccion(String direccion) {this.direccion = direccion; }
+
+    // Método toString para imprimir indormación del contacto
+    public String toString() {
+        return "contacto{" +
+                "nombre=" + nombre + " \'" +
+                ", telefono=" + telefono + " \'" +
+                ", correo" + correo + " \'" +
+        '}';
+    }
 }
